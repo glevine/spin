01fd3504fcac3900e6d1ce9d16d991469a40e5d6
Greg Levine <levine.greg@gmail.com>
2017-01-29 12:48:28 -0500

    Seed the database with data instead of keeping a fully copy of the database in version control

---
+++ data/seed.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+while [ $(curl -s -o /dev/null -w "%{http_code}" -i -H "Accept: application/json; charset=UTF-8" -H "Content-Type: application/json" http://localhost:7474/db/data/) -ne 200 ];
+do
+	echo -n "."
+	sleep 2
+done
+
+curl -s -o /dev/null -i -H "Accept: application/json; charset=UTF-8" -H "Content-Type: application/json" -X POST -d '{"statements": [{"statement": "MATCH (n) DETACH DELETE n"}]}' http://localhost:7474/db/data/transaction/commit
+curl -s -o /dev/null -i -H "Accept: application/json; charset=UTF-8" -H "Content-Type: application/json" -X POST -d '{"statements": [{"statement": "CREATE (google:Resource {name: \"www.google.com\", location: \"http://www.google.com\"}),(yahoo:Resource {name: \"www.yahoo.com\", location: \"http://www.yahoo.com\"}),(espn:Resource {name: \"espn.go.com\", location: \"http://espn.go.com\"}),(cnn:Resource {name: \"www.cnn.com\", location: \"http://www.cnn.com\"}),(news:Tag {name: \"news\"}),(sports:Tag {name: \"sports\"}),(search:Tag {name: \"search\"}),(google)-[:TAGGED]->(search),(yahoo)-[:TAGGED]->(search),(yahoo)-[:TAGGED]->(news),(cnn)-[:TAGGED]->(news),(espn)-[:TAGGED]->(sports),(espn)-[:TAGGED]->(news)"}]}' http://localhost:7474/db/data/transaction/commit
