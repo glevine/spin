7f42a337dc0d8c9e9edc20a01138188da955162c
Greg Levine <levine.greg@gmail.com>
2017-09-20 07:58:11 -0400

    Don't need to check for a connection before installing demo data
    - The script should only be run after the app has been started.
    - The script will fail with a curl error if the database can't be
    reached.

--- data/demo.sh
+++ data/demo.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 
-echo -n "Connecting to database "
-while [ $(curl -s -o /dev/null -w "%{http_code}" -i -H "Accept: application/json; charset=UTF-8" -H "Content-Type: application/json" http://localhost:7474/db/data/) -ne 200 ];
-do
-	echo -n "."
-	sleep 2
-done
-echo " connected"
-
 echo -n "Removing data ... "
 curl -s -o /dev/null -i -H "Accept: application/json; charset=UTF-8" -H "Content-Type: application/json" -X POST -d '{"statements": [{"statement": "MATCH (n) DETACH DELETE n"}]}' http://localhost:7474/db/data/transaction/commit
 echo "done"
 
 echo -n "Populating data ... "
 declare -a STATEMENTS
-while IFS= read -r; do STATEMENTS+=("$REPLY"); done <./data/data.cypher
+while IFS= read -r; do STATEMENTS+=("$REPLY"); done <./data/demo.cypher
 CYPHER=$(echo "$(IFS=,; echo "${STATEMENTS[*]}")" | sed 's/["]/\\&/g')
 curl -i -H "Accept: application/json; charset=UTF-8" -H "Content-Type: application/json" -X POST -d "{\"statements\": [{\"statement\": \"CREATE ${CYPHER}\"}]}" http://localhost:7474/db/data/transaction/commit
 echo "done"
