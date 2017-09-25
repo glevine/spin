8668ee16b9d90181a0bea3f4d1d098552ecc980b
Greg Levine <levine.greg@gmail.com>
2017-09-25 17:52:11 -0400

    Successfully uploading the schema

--- src/index.js
+++ src/index.js
@@ -3,24 +3,78 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const {buildSchema} = require('graphql');
+const fetch = require('node-fetch');
 
-const schema = buildSchema(`
-  type Query {
-    hello: String
+const neo4jUrl = process.env.GRAPHENEDB_URL;
+
+const schema = `
+  type Resource {
+    location: ID!
+    linked_by: [Resource] @relation(name: "LINKED_TO", direction: "in")
+    linked_to: [Resource] @relation(name: "LINKED_TO", direction: "out")
+    topics: [Topic] @relation(name: "TAGGED_WITH")
   }
-`);
 
-const root = {
-  hello: () => {
-    return 'Hello world!';
-  },
-};
+  type Topic {
+    name: ID!
+    resources: [Resource] @relation(name: "TAGGED_WITH")
+  }
+
+  type Query {
+    resource(location:ID!): Resource
+    topic(name:ID!): Topic
+  }
+
+  schema {
+    query: Query
+  }
+`;
+
+function executeQuery(endpoint, options) {
+  const url = neo4jUrl + endpoint;
+
+  options = Object.assign({method: 'POST'}, options);
+
+  return fetch(url, options)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+
+      throw new Error(`Request failed: ${url}`);
+    })
+    .catch((error) => {
+      console.log(error);
+      throw error;
+    });
+}
+
+function uploadSchema(schema) {
+  console.log('Uploading the schema...');
+  executeQuery('graphql/idl', {body: schema}).then((json) => {
+    console.log('Schema has been uploaded!');
+  })
+  .catch((error) => {
+    console.log('Retry upload schema...');
+    setTimeout(() => uploadSchema(schema), 2000);
+  });
+}
+
+uploadSchema(schema);
 
 const app = express();
 
 app.use(graphqlHTTP({
-  schema: schema,
-  rootValue: root,
+  schema: buildSchema(`
+    type Query {
+      hello: String
+    }
+  `),
+  rootValue: {
+    hello: () => {
+      return 'Hello world!';
+    },
+  },
   graphiql: app.get('env') === 'development',
 }));
 
